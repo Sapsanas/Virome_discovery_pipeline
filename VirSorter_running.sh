502a37297f289c5e036f55e5ad720daeba1f17f6
Sana <34066681+Sapsanas@users.noreply.github.com>
2022-07-05 23:35:17 +0200

    Create VirSorter_running.sh

---
+++ VirSorter_running.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=VirSorter
+#SBATCH --output=VirSorter.out
+#SBATCH --error=VirSorter.err
+#SBATCH --mem=8gb
+#SBATCH --time=117:00:00
+#SBATCH --cpus-per-task=4 
+
+
+export PATH=$PATH:/home/umcg-sgarmaeva/.local/bin
+
+##### Dependencies #####
+module load Anaconda3
+
+# Running VirSorter doi:10.7717/peerj.985 #
+#running in decontamination mode based on the results of bacterial genomic dna contamination using alignment to cpn60db
+
+source activate virsorter
+wrapper_phage_contigs_sorter_iPlant.pl \
+	-f ./Roux_analysis/nonredundant_contigs.fasta \
+	--db 2 \
+	--wdir ./Roux_analysis/VirSorter \
+	--ncpu 4 \
+	--virome \
+	--data-dir /groups/umcg-lld/tmp03/umcg-sgarmaeva/virsorter-data \
+	--no_c 1
+
+# VirSorter predicted VLP extraction #
+grep -v "#" ./analysis_outputs/VirSorter/VIRSorter_global-phage-signal.csv | \
+	cut -f1 -d "," | \
+	sed s/VIRSorter_// | \
+	cut -f 1 -d "-" | \
+	sed s/_/./2 | \
+	sed s/_/./8 > ./analysis_outputs/tidy/VIRSorter_global-phage-signal_tidy
+
