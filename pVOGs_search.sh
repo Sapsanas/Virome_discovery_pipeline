83bcb4f0def66236487babaabf28dc0fc2114c50
Sana <34066681+Sapsanas@users.noreply.github.com>
2022-07-05 23:51:30 +0200

    Update pVOGs_search.sh

--- pVOGs_search.sh
+++ pVOGs_search.sh
@@ -13,22 +13,22 @@ mkdir -p ./Roux_analysis/pVOGs
 # Predicting ORFs #
 
 /groups/umcg-lld/tmp03/umcg-sgarmaeva/tools/prodigal/prodigal \
-	-a ./Roux_analysis/pVOGs/nonredundant_contigs.min1000.AA.fasta \
-	-i ./Roux_analysis/nonredundant_contigs.fasta \
+	-a ./Roux_analysis/pVOGs/nonredundant_scaffolds.min1000.AA.fasta \
+	-i ./Roux_analysis/nonredundant_scaffolds.fasta \
 	-p meta \
 	&> ./Roux_analysis/pVOGs/prodigal.log
 
 # Searching for similarities to pVOGs db #
 /groups/umcg-lld/tmp03/umcg-sgarmaeva/tools/hmmer-3.2.1/src/hmmsearch \
 	-E 0.000001 \
-	--tblout ./Roux_analysis/pVOGs/nonredundant_contigs.min1000.AA.tblout \
+	--tblout ./Roux_analysis/pVOGs/nonredundant_scaffolds.min1000.AA.tblout \
 	--cpu 4 \
 	/groups/umcg-lld/tmp03/umcg-sgarmaeva/databases/pvogs/AllvogHMMprofiles/all_vogs.hmm \
-	./Roux_analysis/pVOGs/nonredundant_contigs.min1000.AA.fasta \
+	./Roux_analysis/pVOGs/nonredundant_scaffolds.min1000.AA.fasta \
 	&> ./Roux_analysis/pVOGs/hmmsearch.log
 
 # pVOGs predicted VLP extraction #
-sed -e '1,3d' < ./Roux_analysis/pVOGs/nonredundant_contigs.min1000.AA.tblout | \
+sed -e '1,3d' < ./Roux_analysis/pVOGs/nonredundant_scaffolds.min1000.AA.tblout | \
 	head -n -10 | \
 	awk -F ' ' '{print $1}' | \
 	sort -u | \
