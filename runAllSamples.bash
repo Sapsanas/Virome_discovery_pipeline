0069e1f010e75e446acd98aa8bc5fad06cd6473a
Sana <34066681+Sapsanas@users.noreply.github.com>
2022-07-06 01:10:34 +0200

    Update runAllSamples.bash

--- runAllSamples.bash
+++ runAllSamples.bash
@@ -3,7 +3,8 @@
 for SAMPLE in $@; do
     if [ -d ${SAMPLE} ]; then
         sbatch --output ${SAMPLE}_assembly.out --error ${SAMPLE}_assembly.err --job-name ${SAMPLE} Per_sample_assembly.sh ${SAMPLE} 
+	sbatch --output ${SAMPLE}_dm.out --error ${SAMPLE}_dm.err --job-name ${SAMPLE}_dm dark_matter.sh ${SAMPLE}
     fi
 done
 
-#execution: bash runAllSamples.bash $(cat sample.list)
+#execution: bash runAllSamples.bash $(cat sample.list) or $(cat fragment.list)
